d086e8e06237ebf7b1ddea32cbfcd6b883866a56
alinavit <65871360+alinavit@users.noreply.github.com>
2022-03-20 21:16:26 +0100

    Update auto_predictit_markets_git.py
    added comments for each step. Now we can see at what step the process is atm

--- auto_predictit_markets_git.py
+++ auto_predictit_markets_git.py
@@ -1,32 +1,39 @@
 import requests
-import pandas as pd
-import lxml
-from bs4 import BeautifulSoup
-
 import json
-
 import psycopg2 #POSTGRES
-
+from datetime import datetime
 import db_connect
 
-conn = psycopg2.connect(
-    host = db_connect.UniProd.host,
-    database = db_connect.UniProd.database,
-    user = db_connect.UniProd.user,
-    password = db_connect.UniProd.password
+print('Date:', datetime.now())
 
-)
+try:
+    conn = psycopg2.connect(
+        host = db_connect.UniProd.host,
+        database = db_connect.UniProd.database,
+        user = db_connect.UniProd.user,
+        password = db_connect.UniProd.password
+
+    )
+    print('Connected to db')
+except:
+    print('Error in connection')
+
+
 
 cur = conn.cursor()
+print('Cursor created')
+
 
 url = 'https://www.predictit.org/api/marketdata/all/'
 data_json = requests.get(url, 'json').text
 
 full_json_file = json.loads(data_json)
+print('Files extracted')
 
 json_markets = full_json_file['markets'] #markets are basically questions that should be voted by categrory
 
 ## DB insert series
+new_series = []
 for i in range(len(json_markets)):
     for k in range(len(json_markets[i]['contracts'])):  # contracts are what someone can vote for [DETAILED INFORMATION]
 
@@ -46,9 +53,18 @@ for i in range(len(json_markets)):
         cur.execute(sql_statement, sql_values)
         conn.commit()
 
-        print(cur.rowcount, f"record inserted : {series_id}")
+        #print(cur.rowcount, f"record inserted : {series_id}")
+
+        if cur.rowcount == 1:
+            new_series.append(series_id)
+
+print(f'{len(new_series)} new series have been created: {new_series}')
+
+cur.execute('''select count(distinct series_id) from series_values where series_id like 'predictit%'  ''')
+total_series = cur.fetchall()
 
 ## DB insert series_values
+series_updated = []
 for i in range(len(json_markets)):
     for k in range(len(json_markets[i]['contracts'])):
         series_id = 'predictit_' + str(json_markets[i]['id']) + '_' + str(json_markets[i]['contracts'][k]['id'])
@@ -68,9 +84,16 @@ for i in range(len(json_markets)):
         cur.execute(sql_statement_values, sql_values_values)
         conn.commit()
 
-        print(cur.rowcount, f"inserted: {series_id} for : {json_markets[i]['timeStamp']}")
+        #print(cur.rowcount, f"inserted: {series_id} for : {json_markets[i]['timeStamp']}")
+
+        if cur.rowcount == 1:
+            series_updated.append(series_id)
+
+print(f'{len(series_updated)} out of {total_series[0][0]}  series have been updated')
 
 #close the cursor
 cur.close()
 #close the connection
 conn.close()
+
+print('Disconnected from db')
